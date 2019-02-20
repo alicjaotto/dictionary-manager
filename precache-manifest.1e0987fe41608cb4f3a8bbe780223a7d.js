86a92392f42b67cc6d586e64f763817412e0a6fa
ala <alicja.otto@gmail.com>
2019-02-20 13:12:50 +0100

    Updates

---
+++ precache-manifest.1e0987fe41608cb4f3a8bbe780223a7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "febe876505a374d66c1e",
+    "url": "/static/css/main.a6f9854f.chunk.css"
+  },
+  {
+    "revision": "febe876505a374d66c1e",
+    "url": "/static/js/main.febe8765.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9b2982b07a382dbf74f6",
+    "url": "/static/js/2.9b2982b0.chunk.js"
+  },
+  {
+    "revision": "82e4c6f245f6ef4a8f71d374b7e5e912",
+    "url": "/index.html"
+  }
+];
